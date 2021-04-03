fb90f2ce20ca3ce741e1336c2440a911f3a2f388
Agnes Hardy Bertelsen <laal@itu.dk>
2021-04-03 12:41:49 +0200

    add eval class

---
+++ Evaluations/Evaluator.py
@@ -0,0 +1,11 @@
+class Evaluator:
+
+  def __init__(self, file_name, centrality):
+    self.data = []
+    self.file_name = file_name
+    self.centrality = centrality
+
+  def run_evaluations(self):
+    
+
+  
