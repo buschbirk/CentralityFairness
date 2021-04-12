880b7908bc87ac03f9975fdfc3c6755d7bb8f765
Agnes Hardy Bertelsen <laal@itu.dk>
2021-04-12 14:41:08 +0200

    add evaluator for slices

---
+++ Evaluations/Evaluator-slices.py
@@ -0,0 +1,71 @@
+import os, sys, inspect
+
+import numpy as np
+import pandas as pd
+import math
+import pickle
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+from lenskit.metrics.topnFair import *
+
+CHUNK_SIZE = 100000
+
+class SliceEvaluator:
+
+  def __init__(self, file_name, centrality):
+    self.centrality = centrality
+    self.results = []
+    self.process_data(file_name)
+    # print('gender counts normalized:')
+    # print(self.data.Gender.value_counts(normalize = True))
+    # print('gender counts:')
+    # print(self.data.Gender.value_counts(normalize = False))
+    # self.data.rename(columns = {'AuthorId': 'item', self.centrality: 'rank'}, inplace = True)
+    # self.sort_data()
+    # print('sorted data.head():')
+    # print(self.data.head())
+
+  def sort_data(self, data):
+    if self.centrality == 'Rank':
+      data.sort_values(by = 'rank', ascending = True, inplace = True) 
+    else: 
+      data.sort_values(by = 'rank', ascending = False, inplace = True)
+
+  def process_data(self, path):
+    for i in range(slices):
+      _slice = []
+      for chunk in self.data = pd.read_csv(path, sep = '\t', chunksize = CHUNK_SIZE):
+        _slice.append(chunk[chunk.sliceid==i])
+      df_slice = pd.concat(_slice)
+      df_slice = df_slice.query('Gender != -1')
+      process_slice(df_slice.sample(10000)) # TODO fjern sample
+
+  def process_slice(self, df_slice, slice_index, metrics):
+    df_slice['protected'] = df_slice.Gender.apply(lambda x: int(x == 0))
+    self.sort_data(df_slice)
+    self.add_protected_column(df_slice)
+    evaluations = {'index': slice_index}
+    for m in metrics:
+      evaluations[m] = calculate_fairness(df_slice)
+    self.results.append(evaluations)
+
+  def calculate_fairness(self, df_slice, metric):
+    return calculateNDFairnes(recs=df_slice, truth=[], metric=metric, protected_varible='protected')
+
+  def add_protected_column(self, data):
+    data['protected'] = data.Gender.apply(lambda x: int(x == 0))
+
+  def print_results(self):
+    print(self.results)
+
+if __name__ == '__main__':
+  sample = sys.argv[1]
+  centrality = sys.argv[2]
+  path = sys.argv[3]
+  metrics = ['rND', 'rKL', 'rRD', 'equal_ex']
+  print('Evaluating', path, 'for centrality', centrality)
+  eval = SliceEvaluator(path, centrality)
+  eval.print_results()
