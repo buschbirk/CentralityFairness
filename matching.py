581d6a243c5b19d1db1ab78d0bdb21599b13a284
laal <laal@itu.dk>
2021-04-13 16:03:57 +0200

    Mathcing class

--- matching.py
+++ matching.py
@@ -30,77 +30,109 @@ def read_author_metadata(fos_id=162324750):
     return author_df
 
 
-def match(centrality_df, author_df, random_seed, centrality='PageRank', 
-          max_year_limit=2018, min_year_tolerance=0):
+
+class Matcher():
+
+    def __init__(self, centrality_df, random_seed, max_year_limit=2018, min_year_tolerance=0):
+
+        self.cent_df = centrality_df
+        self.seed = random_seed
+        self.max_year_limit = max_year_limit
+        self.min_year_tolerance = min_year_tolerance
+
+        self.author_df = None
+
+        pd.options.mode.chained_assignment = None
+
+        self.female_population = None
+        self.male_population = None
+
+
+
+    def load_authors(self, fos_id=162324750, folder_destination = "/home/laal/MAG/DATA/AuthorMetadataField.csv"):
+
+        # base_destination = "/home/laal/MAG/DATA/AuthorMetadataField.csv"
         
-    pd.options.mode.chained_assignment = None
+        columns = ['AuthorId', 'FieldOfStudyId', 'Gender', 'MinAffiliationRank', 'NumPapers', 'MinPubDate', 'MaxPubDate', 'PubsPerYear']
         
-    # remove unknown gender
-    dataset = author_df.query("Gender != -1")
-    dataset = dataset[dataset.AuthorId.isin(centrality_df.AuthorId)]
-    
-    # affiliation_quintile 
-    dataset.loc[:,"AffiliationBin"] = pd.qcut(dataset.MinAffiliationRank, 15, labels = False)
-    dataset.loc[:,'MaxYear'] = dataset.MaxPubYear.apply(lambda x: 2021 if x >= max_year_limit else x)
-    
-    male_population = dataset.query("Gender == 1")
-    female_population = dataset.query("Gender == 0")
-    
-    # sample random from opposite gender
-    male_sampled = male_population.sample(female_population.shape[0], replace=False, random_state=random_seed)
-    female_sampled = female_population
-    
-    print("Collected {} female and {} male samples".format(len(male_sampled), len(female_sampled)))
-    
-    ## Compute statistics and visualize    
-    ids = list(male_sampled.AuthorId.values) + list(female_sampled.AuthorId.values)
-    cent_df_sampled = cent_df[cent_df.AuthorId.isin(ids)]
-    
-    _, __ = plot_group_dist(cent_df_sampled, centrality, interval_size=100, max_N=len(cent_df_sampled), protected_group=0, 
-                            unprotected=1, show_unknown=False, field_name='Economics', na_removed=True, 
-                            ax=None, global_rates=None)
-    
-    # sample from population with same starting time and end time
-    sample_females = []
-    sample_males   = []
-    
-    print("Initiating matching on carreer characteristics")
-    
-    # shguffle male and female before sampling
-    
-    for female_record in tqdm(female_population.to_dict('records'), total=len(female_population)):
+        author_df = pd.DataFrame()
         
+        for file in os.listdir(base_destination):
+            if file.endswith('.csv'):
+                df = pd.read_csv(base_destination + "/" + file, names=columns, sep="\t")
+                author_df = pd.concat([author_df, df.query("FieldOfStudyId == {}".format(fos_id))])
         
-        male_filtered = male_population[  (abs(male_population.MinPubYear - female_record['MinPubYear']) <= min_year_tolerance)
-                                        & (male_population.MaxYear == female_record['MaxYear']) 
-                                        & (male_population.AffiliationBin == female_record['AffiliationBin'])]
-        if male_filtered.empty:
-            continue
+        # parse datetimes
+        author_df['MinPubDate'] = pd.to_datetime(author_df['MinPubDate'])
+        author_df['MaxPubDate'] = pd.to_datetime(author_df['MaxPubDate'])
         
-        male_sample = male_filtered.sample(1, random_state=random_seed)
-        # remove sample from male_population
-        male_population.drop(male_sample.index, axis='index', inplace=True)
+        author_df['MinPubYear'] = author_df['MinPubDate'].apply(lambda x: x.year)
+        author_df['MaxPubYear'] = author_df['MaxPubDate'].apply(lambda x: x.year)
         
-        sample_females.append(female_record)
-        sample_males.append(male_sample.iloc[0].to_dict())
+
+        # filter out unknown gender and authors not in centrality dataset
+        self.author_df = author_df.query("Gender != -1")
+        self.author_df = self.author_df[self.author_df.AuthorId.isin(self.centr_df.AuthorId)]
+
+
+        # affiliation_quintile and max year mapping 
+        self.author_df.loc[:,"AffiliationBin"] = pd.qcut(self.author_df.MinAffiliationRank, 15, labels = False)
+        self.author_df.loc[:,'MaxYear'] = self.author_df.MaxPubYear.apply(lambda x: 2021 if x >= self.max_year_limit else x)
+
+        self.male_population = self.author_df.query("Gender == 1")
+        self.female_population = self.author_df.query("Gender == 0") 
+
+
+    def random_sample(self):
+
+        smallest_pop_size = min(self.female_population.shape[0], self.male_population.shape[0])
         
-    print("Found {} matches to {} females".format(len(sample_females), len(female_population)))
-    
-    females_df = pd.DataFrame.from_records(sample_females)
-    males_df = pd.DataFrame.from_records(sample_males)
-    
-    ## Compute statistics and visualize    
-    ids = list(male_sampled.AuthorId.values) + list(female_sampled.AuthorId.values)
-    cent_df_sampled = cent_df[cent_df.AuthorId.isin(ids)]
-    
-    _, __ = plot_group_dist(cent_df_sampled, centrality, interval_size=100, max_N=len(cent_df_sampled), protected_group=0, 
-                            unprotected=1, show_unknown=False, field_name='Economics', na_removed=True, 
-                            ax=None, global_rates=None)
-    
-    return sample_females, sample_males
+        if self.female_population.shape[0]  < self.male_population.shape[0]:
+            male_sampled = self.male_population.sample(self.female_population.shape[0], replace=False, random_state=self.seed)
+            female_sampled = self.female_population     
+        else:
+            male_sampled = self.male_population.sample(frac=1)
+            female_sampled = self.female_population.sample(self.male_population.shape[0], replace=False, random_state=self.seed)   
+
+        return male_sampled, female_sampled   
+
+    def matched_sample(self): 
+        # shuffle male population using seed
+        male_population = self.male_population.sample(frac=1, random_state=random_seed).copy()
+
+        # shuffle female population and iterate over each row as dict (record) 
+        for female_record in tqdm(self.female_population.sample(frac=1, random_state=random_seed).to_dict('records'), 
+                                total=len(self.female_population)):
+            
+            # identify all matching candidates
+            male_filtered = male_population[ 
+                  (abs(male_population.MinPubYear - female_record['MinPubYear']) <= min_year_tolerance) &
+                  (male_population.MaxYear == female_record['MaxYear']) &
+                  (male_population.AffiliationBin == female_record['AffiliationBin'])
+                ]
+            
+            # if no candidates found, continue to next record
+            if male_filtered.empty:
+                continue
+            
+            # sample a single record
+            male_sample = male_filtered.sample(1, random_state=self.seed)
+            
+            # remove sample from male_population
+            male_population.drop(male_sample.index, axis='index', inplace=True)
+            
+            # append male and female samples as dicts
+            sample_females.append(female_record)
+            sample_males.append(male_sample.iloc[0].to_dict())
+            
+
+        print("Found {} matches to {} females".format(len(sample_females), len(female_population)))
+        
+        females_df = pd.DataFrame.from_records(sample_females)
+        males_df = pd.DataFrame.from_records(sample_males)        
+
+        return females_df, males_df 
+
 
 if __name__ == '__main__':
-    author_df = read_author_metadata()
-    cent_df = pd.read_csv("/home/laal/MAG/DATA/NETWORKS/SimpleWeightEconomics2020CentralityGendered.csv", sep="\t")
-    test = match(cent_df, author_df, random_seed=42, centrality='PageRank', 
-                 max_year_limit=2018, min_year_tolerance=0)
+    pass
